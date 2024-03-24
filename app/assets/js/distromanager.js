fb9637c40b92576a11a67b8cf2f5e03d501723b9
go-tiger <inter_tiger_@naver.com>
2024-03-25 07:09:47 +0900

    feat: Test 0.0.3 Release

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -5,7 +5,7 @@ const ConfigManager = require('./configmanager');
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
 exports.REMOTE_DISTRO_URL =
-    'http://gotiger.ipdisk.co.kr:99/publist/HDD1/Launcher/Handmade/distribution.json';
+    'http://gotiger.ipdisk.co.kr:9999/launcher/distribution/distribution-teamhm.json';
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
